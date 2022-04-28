9a219621e18732490e414f336cdfc367408738fa
kjj13 <rlawndwo1305@naver.com>
2022-04-28 17:24:45 +0900

    feat: Catalogs Microservice, Orders Microservice 작성

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'Berlin', 100, 1500),
+          ('CATALOG-002', 'Tokyo', 110, 1000),
+          ('CATALOG-003', 'Seoul', 120, 2000);
