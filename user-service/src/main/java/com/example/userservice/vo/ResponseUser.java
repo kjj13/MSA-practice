e0991e58c2243e78455ca2e8c1afc310f4038e32
kjj13 <rlawndwo1305@naver.com>
2022-04-28 11:13:24 +0900

    feat: Spring Cloud Gateway와 User Microservice 연동

--- user-service/src/main/java/com/example/userservice/vo/ResponseUser.java
+++ user-service/src/main/java/com/example/userservice/vo/ResponseUser.java
@@ -1,10 +1,16 @@
 package com.example.userservice.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)  // NULL인 데이터는 포함하지 않게 함.
 public class ResponseUser {
     private String email;
     private String name;
     private String userId;
+
+    private List<ResponseOrder> orders;
 }
